ec42e2f8970ef4f6a927a8638f3a8c514daa3119
yujiarong <yujiarong@sailvan.com>
2018-05-18 16:33:06 +0800

    净化

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -41,59 +41,12 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-    public function login(Request $request)
-    {
-        $this->validateLogin($request);
-        $email     = $request->get('email');
-        $password  = $request->get('password');
-        $count     = User::where('email',$email)->count();
-        if($count <1){
-            $param              = ["user_name"=>$email,"pwd"=>$password];
-            $param['method']    = 'power.user.login.get';
-            $result     = json_decode(callOpenSystem($param),true);
-            if(empty($result)){
-                return redirect()->route('login')->withInput()->withErrors('开放系统连接失败');
-            }
-            if(isset($result['errCode'])){
-                return redirect('/login')->withInput()->withErrors($result['errMsg']);            
-            }
-            if(!isset($result['userName']) ){
-                session()->flush();
-                return redirect('/login')->withInput()->withErrors('登陆失败');
-            }
-            //erp 账号登陆成功 进行手动添加
-            if(!$this->store(['email'=>$email,'name'=>$result['userCnName'],'password'=>$password ]) ){
-                return redirect('/login')->withInput()->withErrors('手动注册失败');            
-            }
-
-        }
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the login attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if ($this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
-
-            return $this->sendLockoutResponse($request);
-        }
-
-        if ($this->attemptLogin($request)) {
-            return $this->sendLoginResponse($request);
-        }
-
-        // If the login attempt was unsuccessful we will increment the number of attempts
-        // to login and redirect the user back to the login form. Of course, when this
-        // user surpasses their maximum number of attempts they will get locked out.
-        $this->incrementLoginAttempts($request);
-
-        return $this->sendFailedLoginResponse($request);
-    }
 
     public function store($reData){
         $userModel = new User;
         $userModel->email    = $reData['email'];
         $userModel->name     = $reData['name'];
         $userModel->password = bcrypt($reData['password']);
-        // $userModel->power    = 'saler';
         return  $userModel->save();      
 
     }    
--- app/Http/Middleware/RouteNeedsPermission.php
+++ app/Http/Middleware/RouteNeedsPermission.php
@@ -30,25 +30,6 @@ class RouteNeedsPermission
 
     public function handle($request, Closure $next, $role, $needsAll = false)
     {
-        $roleArr      = explode(';', $role);
-        $userRole     = Auth::user()->power;
-        $routeName    = Route::currentRouteName();
-        if(explode('.',$routeName)[0] == 'user' && !in_array(auth_user()->name, ['余嘉榕','liyuhua']) ){
-            return redirect()->back()->withFlashDanger('您没有权限进入此模块');
-        }
-        $paypal_access = config('access.paypal_access');
-        // print_r($paypal_access);
-        // print_r(auth_user()->name);
-        if(explode('.',$routeName)[0] == 'pay' && !in_array(auth_user()->name, $paypal_access) ){
-            return redirect()->back()->withFlashDanger('您没有权限进入支付模块');
-        }
-        if(!in_array($userRole, $roleArr)){
-            if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'){
-                return response()->json(['errCode'=>'101','errMsg'=>'您没有权限进行此操作']);
-            }else{
-                return redirect()->back()->withFlashDanger('您没有权限进行此操作');
-            }
-        }
         return $next($request);
     }
 
